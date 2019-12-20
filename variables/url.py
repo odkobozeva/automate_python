8a1f2a42c8f5f5e7c14abe897f1d25fdb709a072
odkobozeva <odkobozeva@gmail.com>
2019-12-20 16:42:25 +0300

    add variables folder

---
+++ variables/url.py
@@ -0,0 +1,27 @@
+url_2bitcoins_response = ['https://2bitcoins.ru/',
+                        'https://2bitcoins.ru/coins/',
+                        'https://2bitcoins.ru/coins/ethereum/',
+                        'https://2bitcoins.ru/tag/birzha/',
+                        'https://2bitcoins.ru/author/valekas/',
+                        'https://2bitcoins.ru/bitkoin-ne-samaja-populyarnaja-kriptovalyuta/']
+
+url_hinews_response = ['https://hi-news.ru/',
+                       'https://hi-news.ru/auto',
+                       'https://hi-news.ru/tag/mars',
+                       'https://hi-news.ru/author/liubov',
+                       'https://hi-news.ru/space/stolknovenie-nejtronnyx-zvezd-obrazuet-stroncij-element-tyazhelee-zheleza.html',
+                       'https://hi-news.ru/business-analitics/slabaya-videokarta-ne-prigovor-kak-i-skolko-segodnya-mozhno-zarabatyvat-s-deshyovoj-kartoj-ot-nvidia-ili-amd.html']
+
+url_android_response = ['https://androidinsider.ru/',
+                        'https://androidinsider.ru/soft',
+                        'https://androidinsider.ru/tag/android-10',
+                        'https://androidinsider.ru/author/gellert',
+                        'https://androidinsider.ru/eto-interesno/kak-starye-smartfony-nexus-prevrashhayut-v-ustrojstva-dlya-zashhishhennoj-svyazi.html',
+                        'https://androidinsider.ru/smartfony/samsung-nas-obmanula-galaxy-note-10-ne-takoj-kak-nam-obeshhali.html']
+
+url_apple_response = ['https://appleinsider.ru/',
+                      'https://appleinsider.ru/ios',
+                      'https://appleinsider.ru/tag/iphone-11-pro',
+                      'https://appleinsider.ru/author/legenda_roka',
+                      'https://appleinsider.ru/accessories/kak-stat-blogerom.html',
+                      'https://appleinsider.ru/iphone/chto-my-zhdem-ot-prezentacii-apple-iphone-11-iphone-11-pro-i-iphone-11-pro-max.html']
