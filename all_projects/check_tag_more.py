b51679f986243e2c300cf4b747af77cedba95bc4
odkobozeva <odkobozeva@gmail.com>
2020-01-08 17:39:06 +0300

    changed the type of variable

--- all_projects/check_tag_more.py
+++ all_projects/check_tag_more.py
@@ -22,20 +22,19 @@ def test_main(url):
 
 def tag_is_exist(url):
     if f'{url}' == 'https://hi-news.ru/':
-        find_post = ('div', 'type-post')
-        find_link_more = ('a', 'more-link')
+        find_post = {'tag': 'div', 'class': 'type-post'}
+        find_link_more = {'tag': 'a', 'class': 'more-link'}
 
     elif f'{url}' == 'https://appleinsider.ru/':
-        find_post = ('article', '')
-        find_link_more = ('a', 'btn-outline')
-
+        find_post = {'tag': 'article', 'class': ''}
+        find_link_more = {'tag': 'a', 'class': 'btn-outline'}
     elif f'{url}' == 'https://androidinsider.ru/':
-        find_post = ('div', 'post-title')
-        find_link_more = ('a', 'more-link')
+        find_post = {'tag': 'div', 'class': 'post-title'}
+        find_link_more = {'tag': 'a', 'class': 'more-link'}
     flag = True
     response = requests.get(f'{url}').text
-    articles = BeautifulSoup(response, 'html.parser').find_all(find_post[0], find_post[1])
-    link_more = BeautifulSoup(response, 'html.parser').find_all(find_link_more[0], find_link_more[1])
+    articles = BeautifulSoup(response, 'html.parser').find_all(find_post['tag'], find_post['class'])
+    link_more = BeautifulSoup(response, 'html.parser').find_all(find_link_more['tag'], find_link_more['class'])
     post_url = [article.a['href'] for article in articles]
     post_link_more = [link['href'] for link in link_more]
     for i in post_url:
