6577357ee1ebe6d2922280d4234cb8fc6b0681c9
odkobozeva <odkobozeva@gmail.com>
2020-01-20 17:42:27 +0300

    add conftest.py

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+import variables.url
+
+
+def pytest_addoption(parser):
+    parser.addoption("--env", action="store")
+    parser.addoption("--url", action="store", default=None, help="Choose url: hinews, android, apple, 2bitcoins")
+
+
+@pytest.fixture(scope='session')
+def env(request):
+    env_value = request.config.option.env
+    if env_value == 'dev':
+        env_value = 'http://beta:ateb@dev.'
+    else:
+        env_value = 'https://'
+    return env_value
+
+
+@pytest.fixture(scope='session')
+def url_amp(request):
+    url_value = request.config.option.url
+    if url_value == '2bitcoins':
+        url_value = variables.url.url_bitcoins_amp
+    elif url_value == 'android':
+        url_value = variables.url.url_android_amp
+    elif url_value == 'apple':
+        url_value = variables.url.url_apple_amp
+    elif url_value == 'hinews':
+        url_value = variables.url.url_hinews_amp
+    else:
+        raise pytest.UsageError("--url must be: hinews, android, apple, 2bitcoins")
+    return url_value
