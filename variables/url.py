dc816b58582242fe13d33092fd50091bab6eeea4
odkobozeva <odkobozeva@gmail.com>
2020-01-20 17:43:35 +0300

    update variables.url

--- variables/url.py
+++ variables/url.py
@@ -25,3 +25,33 @@ url_apple_response = ['https://appleinsider.ru/',
                       'https://appleinsider.ru/author/legenda_roka',
                       'https://appleinsider.ru/accessories/kak-stat-blogerom.html',
                       'https://appleinsider.ru/iphone/chto-my-zhdem-ot-prezentacii-apple-iphone-11-iphone-11-pro-i-iphone-11-pro-max.html']
+
+url_hinews_amp = ['hi-news.ru/amp/',
+          'hi-news.ru/author/thehalo/amp',
+          'hi-news.ru/tag/tesla/amp',
+          'hi-news.ru/eto-interesno/amp',
+          'hi-news.ru/about/amp',
+          'hi-news.ru/advert/amp']
+
+url_android_amp = ['androidinsider.ru/amp/',
+          'androidinsider.ru/author/gellert/amp',
+          'androidinsider.ru/tag/galaxy-s10-2/amp',
+          'androidinsider.ru/soft/amp',
+          'androidinsider.ru/about/amp',
+          'androidinsider.ru/advert/amp']
+
+url_apple_amp = ['appleinsider.ru/amp/',
+          'appleinsider.ru/author/gellert/amp',
+          'appleinsider.ru/tag/iphone-xs/amp',
+          'appleinsider.ru/accessories/amp',
+          'appleinsider.ru/about/amp',
+          'appleinsider.ru/advert/amp']
+
+url_bitcoins_amp = ['2bitcoins.ru/amp/',
+          '2bitcoins.ru/tag/majning/amp/',
+          '2bitcoins.ru/coins/ethereum/amp/',
+          '2bitcoins.ru/author/valekas/amp/',
+          '2bitcoins.ru/kriptovaljutnaja-biblija/amp/',
+          '2bitcoins.ru/coins/amp/',
+          '2bitcoins.ru/reklama/amp/',
+          '2bitcoins.ru/kontakty/amp/']
