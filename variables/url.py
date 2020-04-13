0e8c094849f176113f6a786c0a1941871da30ddb
odkobozeva <odkobozeva@gmail.com>
2020-04-13 09:13:04 +0300

    update variables

--- variables/url.py
+++ variables/url.py
@@ -48,10 +48,78 @@ url_apple_amp = ['appleinsider.ru/amp/',
           'appleinsider.ru/advert/amp']
 
 url_bitcoins_amp = ['2bitcoins.ru/amp/',
-          '2bitcoins.ru/tag/majning/amp/',
+          '2bitcoins.ru/tag/mining/amp/',
           '2bitcoins.ru/coins/ethereum/amp/',
           '2bitcoins.ru/author/valekas/amp/',
           '2bitcoins.ru/kriptovaljutnaja-biblija/amp/',
           '2bitcoins.ru/coins/amp/',
           '2bitcoins.ru/reklama/amp/',
           '2bitcoins.ru/kontakty/amp/']
+
+
+url_apple_with_robots = ['appleinsider.ru/',
+                        'appleinsider.ru/ios',
+                        'appleinsider.ru/tag/iphone-11-pro',
+                        'appleinsider.ru/accessories/kak-stat-blogerom.html',
+                        'appleinsider.ru/iphone/chto-my-zhdem-ot-prezentacii-apple-iphone-11-iphone-11-pro-i-iphone-11-pro-max.html',
+                        'appleinsider.ru/author/gellert',
+                        'appleinsider.ru/tags',
+                        'appleinsider.ru/about',
+                        'appleinsider.ru/advert',
+                        # 'https://appleinsider.ru/fsef'
+]
+
+
+url_apple_without_robots = ['appleinsider.ru/page/5',
+                        'appleinsider.ru/ios/page/5',
+                        'appleinsider.ru/tag/iphone-11-pro/page/5'
+]
+
+url_hinews_with_robots = ['hi-news.ru/',
+                        'hi-news.ru/space',
+                        'hi-news.ru/tag/kosmos',
+                        'hi-news.ru/eto-interesno/francuzy-xotyat-otpravit-vino-v-kosmos-zachem.html',
+                        'hi-news.ru/gadgets/pochemu-chajnik-shumit-vo-vremya-raboty.html',
+                        'hi-news.ru/author/bogdanov',
+                        'hi-news.ru/tags',
+                        'hi-news.ru/about',
+                        'hi-news.ru/advert'
+]
+
+url_hinews_without_robots = ['hi-news.ru/page/5',
+                        'hi-news.ru/space/page/5',
+                        'hi-news.ru/tag/kosmos/page/5',
+]
+
+url_android_with_robots = ['androidinsider.ru/',
+                        'androidinsider.ru/smartfony',
+                        'androidinsider.ru/tag/xiaomi',
+                        'androidinsider.ru/eto-interesno/kak-ponyat-chto-izobrazhenie-bylo-otfotoshopleno.html',
+                        'androidinsider.ru/os/kak-obnovit-smartfony-huawei-na-android-10.html',
+                        'androidinsider.ru/author/gellert',
+                        'androidinsider.ru/tags',
+                        'androidinsider.ru/about',
+                        'androidinsider.ru/advert'
+]
+
+url_android_without_robots = ['androidinsider.ru/page/50',
+                        'androidinsider.ru/smartfony/page/10',
+                        'androidinsider.ru/tag/xiaomi/page/10',
+]
+
+
+
+meta_descr = {"appleinsider.ru/" : "Один из самых популярных в России веб-ресурсов, посвященных всему, что связано с компанией Apple. Самые горячие и интересные новости о продукции Apple.",
+"appleinsider.ru/ios" : "Откройте для себя целую вселенную iOS благодаря нашим свежим новостям и интересным обзорам! Ни одно обновление этой мобильной платформы не пройдет мимо вашег...",
+"appleinsider.ru/tag/iphone-11-pro" : "iPhone 11 Pro — улучшенная версия iPhone 11. Смартфон оснащен тройной камерой, которая позволяет ему делать широкоугольные снимки, но это не единственн...",
+"appleinsider.ru/accessories/kak-stat-blogerom.html" : "Мы живем в такое время, когда телевидение хоть и продолжает жить, но постепенно пере...",
+"appleinsider.ru/iphone/chto-my-zhdem-ot-prezentacii-apple-iphone-11-iphone-11-pro-i-iphone-11-pro-max.html" : "Наконец мы приблизились к финишной черте. Совсем скоро компания Apple покажет нам устройства, о которых мы с ...",
+"appleinsider.ru/author/gellert" : "Несостоявшийся юрист, волею судеб и главного редактора оказавшийся в должности автора на AppleInsider.ru, а потом и AndroidInsider.ru. Искренне считает, что разбирается в те\
+хнике Apple не хуже самого Тима Кука и очень радуется, когда главред называет его орлом. Обладает глубокими познаниями в вопросах защиты прав потребителей. Сотрудничает с Медиагруппой i10.ru с 2015 года. Увлеч\
+ения: инвестиции, научпоп, авиация.",
+"appleinsider.ru/tags" : 'None',
+"appleinsider.ru/about" : "None",
+"appleinsider.ru/advert" : "None",
+"appleinsider.ru/page/5" : "Один из самых популярных в России веб-ресурсов, посвященных всему, что связано с компанией Apple. Самые горячие и интересные новости о продукции Apple.",
+"appleinsider.ru/ios/page/5" : "Откройте для себя целую вселенную iOS благодаря нашим свежим новостям и интересным обзорам! Ни одно обновление этой мобильной платформы не пройдет мимо вашег...",
+"appleinsider.ru/tag/iphone-11-pro/page/5" : "iPhone 11 Pro — улучшенная версия iPhone 11. Смартфон оснащен тройной камерой, которая позволяет ему делать широкоугольные снимки, но это не единственн..."}
