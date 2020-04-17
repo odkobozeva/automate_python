ce93263c38b9296eec0a12f28a0b9416a41c7f7c
odkobozeva <odkobozeva@gmail.com>
2020-04-17 13:26:34 +0300

    add variables url_2bitcoins.py

---
+++ variables/url_2bitcoins.py
@@ -0,0 +1,12 @@
+pagespeed = [
+    'https://2bitcoins.ru/',
+    'https://2bitcoins.ru/page/4/',
+    'https://2bitcoins.ru/author/neilarmstrong5200/',
+    'https://2bitcoins.ru/tag/mining/',
+    'https://2bitcoins.ru/tag/mining/page/2/',
+    'https://2bitcoins.ru/coins/',
+    'https://2bitcoins.ru/coins//page/3/',
+    'https://2bitcoins.ru/opasnost-kriptomoshennikov-v-pandemii/',
+    'https://2bitcoins.ru/otpravit-kriptovalyutu-bez-interneta/',
+]
+
