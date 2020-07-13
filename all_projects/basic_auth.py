afa903f0408e7174287a4a27367fa8b0c66430a1
odkobozeva <odkobozeva@gmail.com>
2020-07-13 14:14:43 +0300

    update basic_auth.py

--- all_projects/basic_auth.py
+++ all_projects/basic_auth.py
@@ -4,34 +4,47 @@ import allure
 
 
 @allure.feature('Проверка работы сервера и базовой авторизации')
-@pytest.mark.parametrize('url', ['http://webdev.2miners.com/',
-                                 'https://eth.dev.2miners.com/en',
-                                 'http://dev.2bitcoins.ru/',
-                                 'http://dev.hi-news.ru/',
-                                 'http://dev.appleinsider.ru/',
-                                 'http://dev.androidinsider.ru/',
-                                 'https://staging.2masternodes.com/'])
-def test_main(url):
+@pytest.mark.parametrize('url', ['http://webdev.2miners.com/'])
+def test_basic_auth_2miners(url):
+    main(url)
+
+
+@allure.feature('Проверка работы сервера и базовой авторизации')
+@pytest.mark.parametrize('url', ['https://eth.webdev.2miners.com/en'])
+def test_basic_auth_2minersdash(url):
+    main(url)
+
+@allure.feature('Проверка работы сервера и базовой авторизации')
+@pytest.mark.parametrize('url', ['http://dev.2bitcoins.ru/'])
+def test_basic_auth_2bitcoins(url):
+    main(url)
+
+
+@allure.feature('Проверка работы сервера и базовой авторизации')
+@pytest.mark.parametrize('url', ['http://dev.appleinsider.ru/'])
+def test_basic_auth_apple(url):
+    main(url)
+
+
+@allure.feature('Проверка работы сервера и базовой авторизации')
+@pytest.mark.parametrize('url', ['http://dev.androidinsider.ru/'])
+def test_basic_auth_android(url):
+    main(url)
+
+
+@allure.feature('Проверка работы сервера и базовой авторизации')
+@pytest.mark.parametrize('url', ['http://dev.hi-news.ru/'])
+def test_basic_auth_hinews(url):
+    main(url)
+
+
+def main(url):
     print(f'Проверяем {url}')
     if server_is_available(url) and basic_auth(url):
         print('Status - Passed')
     else:
         print('Status - FAIL')
         pytest.fail('Basic Authentication - failed')
-
-
-def basic_auth(url):
-    status = 0
-    try:
-        status = requests.get(f'{url}').status_code
-        with allure.step(f'Проверяем базовую авторизацию - {url}'):
-            assert status == 401
-    except AssertionError:
-        print('Базовая авторизация отключена')
-    except:
-        print('Не удалось получить статус')
-    finally:
-        return status == 401
 
 
 def server_is_available(url):
@@ -51,3 +64,17 @@ def server_is_available(url):
         print('Server Error')
     finally:
         return status_auth == 200
+
+
+def basic_auth(url):
+    status = 0
+    try:
+        status = requests.get(f'{url}').status_code
+        with allure.step(f'Проверяем базовую авторизацию - {url}'):
+            assert status == 401
+    except AssertionError:
+        print('Базовая авторизация отключена')
+    except:
+        print('Не удалось получить статус')
+    finally:
+        return status == 401
